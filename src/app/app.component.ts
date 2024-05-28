32eb20c81cc89eb8879ab511bee0c8f5c87ad235
= <=>
2024-05-28 11:58:30 -0600

    Revert "Snapshot final seccion 12"
    This reverts commit a200e79f4ceeda1702d25e57d33a83208939f7e2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuItem } from 'primeng/api';
-
-import { MenuComponent } from './shared/components/menu/menu.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-// Configuracion del locale de la app
-// import localeEsMX from '@angular/common/locales/es-MX'
-// import localeFrCA from '@angular/common/locales/fr-CA'
-
-// import { registerLocaleData } from '@angular/common'
-
-// registerLocaleData(localeEsMX);
-// registerLocaleData(localeFrCA);
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    MenuComponent,
-
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pipesApp';
-  items: MenuItem[] = [];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import path from 'path';
 
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./products/products.routes').then(m => m.pagesRoutes)
-  }
-];
+export const routes: Routes = [];
